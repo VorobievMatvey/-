207c5878ba84cf123fd75e7c7df51d9cc28a0fa6
Matvey Vorobiev <92922300+VorobievMatvey@users.noreply.github.com>
2021-12-24 17:31:32 +0300

    Delete NewCat.py

--- NewCat.py
+++
@@ -1,7 +0,0 @@
-from cat import Cat
-
-cat_1 = Cat('Барон', 'Мальчик', '2 года')
-cat_2 = Cat('Сэм', 'Мальчик', '2 года')
-
-print(f"Первый кот - Имя: {cat1.name}, Пол: {cat1.sex}, Возраст: {cat1.age}")
-print(f"Второй кот - Имя: {cat2.name}, Пол: {cat2.sex}, Возраст: {cat2.age}")
